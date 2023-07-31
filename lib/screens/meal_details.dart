592e718e0275b2b0214e8e3906e06dc7b3339d14
Navid <navidrahman92@gmail.com>
2023-07-31 12:05:58 +0600

    Meals details screen information updated

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -28,11 +28,83 @@ class MealDetailsScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Image.network(
-        meal.imageUrl,
-        height: 300,
-        width: double.infinity,
-        fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            const Text(
+              'Ingredients',
+              style: TextStyle(
+                color: Color.fromARGB(255, 245, 166, 69),
+                fontSize: 24,
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Column(
+              children: meal.ingredients.map((ingredient) {
+                return Column(
+                  children: [
+                    Text(
+                      ingredient,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                  ],
+                );
+              }).toList(),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            const Text(
+              'Steps',
+              style: TextStyle(
+                color: Color.fromARGB(255, 245, 166, 69),
+                fontSize: 24,
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Column(
+              children: meal.steps.map((step) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        step,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            )
+          ],
+        ),
       ),
     );
   }
