34fb1e779ba4286cbbfd0e20e0923114884c4983
Navid <navidrahman92@gmail.com>
2023-07-31 17:14:17 +0600

    Favourites provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/screens/tabs.dart';
 import 'package:meals_app/utils/theme_data.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MealsApp());
+  runApp(
+    const ProviderScope(
+      child: MealsApp(),
+    ),
+  );
 }
 
 class MealsApp extends StatelessWidget {
